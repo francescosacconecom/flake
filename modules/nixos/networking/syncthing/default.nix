72ddbebce17a268775856b920da4ae0e5ce31365
Francesco Saccone <francesco@francescosaccone.com>
2025-01-31 16:47:26 +0100

    Fix issues caused by 9f1c769 and reformat

--- modules/nixos/networking/syncthing/default.nix
+++ modules/nixos/networking/syncthing/default.nix
@@ -9,7 +9,8 @@
   options.modules.networking.syncthing = {
     enable = lib.mkOption {
       description = "Whether to enable Syncthing.";
-      default = false; type = lib.types.bool;
+      default = false;
+      type = lib.types.bool;
     };
     root = lib.mkOption {
       description = "The root directory where Syncthing folders will reside.";
@@ -25,22 +26,23 @@
     folders = lib.mkOption {
       description = "For each folder ID, its configuration.";
       default = { };
-      type = lib.types.submodule {
-        options = {
-          devices = lib.mkOption {
-            description = "The list of device IDs which will have access to the folder.";
-            type = lib.types.listOf lib.types.str;
+      type =
+        lib.types.submodule {
+          options = {
+            devices = lib.mkOption {
+              description = "The list of device IDs which will have access to the folder.";
+              type = lib.types.listOf lib.types.str;
+            };
           };
-        };
-      }
-      |> lib.types.attrsOf;
+        }
+        |> lib.types.attrsOf;
     };
   };
 
   config = lib.mkIf config.modules.networking.syncthing.enable {
     users.users.syncthing = {
       hashedPassword = "!";
-      isNormalUser = true;
+      isSystemUser = true;
       group = "syncthing";
       createHome = true;
       home = config.modules.networking.syncthing.root;
@@ -55,7 +57,6 @@
       user = "syncthing";
       group = "syncthing";
 
-      urAccepted = -1;
       openDefaultPorts = false;
       relay.enable = false;
       systemService = true;
@@ -63,14 +64,17 @@
       dataDir = config.modules.networking.syncthing.root;
 
       settings = {
-        options = let
-          inherit (config.modules.networking.syncthing) announce;
-        in {
-          relaysEnabled = true;
-          localAnnounceEnabled = announce.enable;
-          localAnnouncePort = lib.mkIf announce.enable 21027;
-          limitBandwidthInLan = false;
-        };
+        options =
+          let
+            inherit (config.modules.networking.syncthing) announce;
+          in
+          {
+            urAccepted = -1;
+            relaysEnabled = true;
+            localAnnounceEnabled = announce.enable;
+            localAnnouncePort = lib.mkIf announce.enable 21027;
+            limitBandwidthInLan = false;
+          };
         folders =
           config.modules.networking.syncthing.folders
           |> builtins.mapAttrs (
@@ -85,31 +89,32 @@
             }
           );
         devices =
-            config.modules.networking.syncthing.folders
-            |> builtins.attrValues
-            |> builtins.map (folderConfig: folderConfig.devices)
-            |> builtins.concatLists
-            |> builtins.map (
-              device: {
-                name = device;
-                value = {
-                  autoAcceptFolders = true;
-                  id = device;
-                };
-              }
-            )
-            |> builtins.listToAttrs;
+          config.modules.networking.syncthing.folders
+          |> builtins.attrValues
+          |> builtins.map (folderConfig: folderConfig.devices)
+          |> builtins.concatLists
+          |> builtins.map (device: {
+            name = device;
+            value = {
+              autoAcceptFolders = true;
+              id = device;
+            };
+          })
+          |> builtins.listToAttrs;
       };
     };
 
     networking.firewall.allowedTCPPorts =
       [
         22000
-      ] ++ if config.modules.networking.syncthing.announce.enable then
-        [
-          21027
-        ]
-      else
-        [ ];
+      ]
+      ++ (
+        if config.modules.networking.syncthing.announce.enable then
+          [
+            21027
+          ]
+        else
+          [ ]
+      );
   };
 }
