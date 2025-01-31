francescosacconecom/flake

commit 9f1c769797bd7728ffa54cf1eb8a8e25e7eef78c

[server] Implement Syncthing module

hosts/server/default.nix
@@ -160,6 +160,20 @@
           git = root;
         };
       };
+      syncthing = {
+        enable = true;
+        root = "/var/lib/syncthing";
+        announce = {
+          enable = true;
+        };
+        folders = {
+          music = {
+            devices = [
+              "AXH5A4N-C5MEHNR-AKFFXUO-CRNLEFI-XAGJ23U-25MIBSG-2WHJMZO-K35GHQF";
+            ];
+          };
+        };
+      };
     };
   };
 

commit 488cd8009d841668dd46298ffcf103425c1e7a8a

Fix error caused by 9f1c769 and add a device to Syncthing

hosts/server/default.nix
@@ -169,7 +169,8 @@
         folders = {
           music = {
             devices = [
-              "AXH5A4N-C5MEHNR-AKFFXUO-CRNLEFI-XAGJ23U-25MIBSG-2WHJMZO-K35GHQF";
+              "AXH5A4N-C5MEHNR-AKFFXUO-CRNLEFI-XAGJ23U-25MIBSG-2WHJMZO-K35GHQF"
+              "EWZK7V3-2LU7653-G25DOIA-KGGTSVR-GOHDYLU-F7EHMPQ-5P2OPUR-QIHBTAH"
             ];
           };
         };