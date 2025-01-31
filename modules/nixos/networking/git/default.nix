francescosacconecom/flake

commit 242343d6c72186a2d8792c2d348f232268500652

[nixos-module] [server] Add Git daemon option to module

modules/nixos/networking/git/default.nix
@@ -12,6 +12,13 @@
       default = false;
       type = lib.types.bool;
     };
+    daemon = {
+      enable = lib.mkOption {
+        description = "Whether to enable Stagit.";
+        default = false;
+        type = lib.types.bool;
+      };
+    };
     stagit = {
       enable = lib.mkOption {
         description = "Whether to enable Stagit.";
@@ -52,6 +59,26 @@
         init.defaultBranch = "master";
       };
     };
+
+    services.gitDaemon =
+      let
+        inherit (config.modules.networking.git) daemon;
+      in
+      lib.mkIf daemon.enable {
+        enable = true;
+        package = pkgs.git;
+
+        user = "git";
+        group = "git";
+        basePath = "/srv/git";
+        exportAll = true;
+
+        port = 9418;
+      };
+
+    networking.firewall.allowedTCPPorts = lib.mkIf config.modules.networking.git.daemon [
+      9418
+    ];
 
     systemd =
       let

hosts/server/default.nix
@@ -115,6 +115,9 @@
       };
       git = {
         enable = true;
+        daemon = {
+          enable = true;
+        };
         stagit = rec {
           enable = true;
           root = "/var/www/git";

commit ae64dc1558898838d4d7bc84e63df494bdd6fc78

Fix errors caused by 242343d

modules/nixos/networking/git/default.nix
@@ -43,7 +43,7 @@
   config = lib.mkIf config.modules.networking.git.enable {
     users.users.git = {
       hashedPassword = "!";
-      isNormalUser = true;
+      isSystemUser = true;
       group = "git";
       createHome = true;
       home = "/srv/git";
@@ -76,7 +76,7 @@
         port = 9418;
       };
 
-    networking.firewall.allowedTCPPorts = lib.mkIf config.modules.networking.git.daemon [
+    networking.firewall.allowedTCPPorts = lib.mkIf config.modules.networking.git.daemon.enable [
       9418
     ];
 