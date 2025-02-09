d7f1e411cc78d47f5652df4e28f62b4b6ec2aa0f
Francesco Saccone <francesco@francescosaccone.com>
2025-02-09 13:45:09 +0100

    [pandoc] Add components option

--- modules/nixos/pandoc/default.nix
+++ modules/nixos/pandoc/default.nix
@@ -22,6 +22,23 @@
       description = "The directory containing the Markdown files.";
       type = lib.types.uniq lib.types.path;
     };
+    components = {
+      head = lib.mkOption {
+        description = "The HTML <head> content.";
+        default = pkgs.writeText "head.html" "";
+        type = lib.types.uniq lib.types.path;
+      };
+      header = lib.mkOption {
+        description = "The HTML <header> content.";
+        default = pkgs.writeText "header.html" "";
+        type = lib.types.uniq lib.types.path;
+      };
+      footer = lib.mkOption {
+        description = "The HTML <footer> content.";
+        default = pkgs.writeText "footer.html" "";
+        type = lib.types.uniq lib.types.path;
+      };
+    };
   };
 
   config = lib.mkIf config.modules.pandoc.enable {
@@ -65,14 +82,31 @@
             Type = "oneshot";
             ExecStart =
               let
-                inherit (config.modules.pandoc) input output;
+                inherit (config.modules.pandoc) input output components;
                 script = pkgs.writeShellScriptBin "pandoc" ''
                   ${pkgs.findutils}/bin/find "${input}" -name '*.md' | while read -r md_file; do
                     relative_path="${"$"}{md_file#${input}/}"
                     output_file="${output}/${"$"}{relative_path%.md}.html"
 
                     ${pkgs.coreutils}/bin/mkdir -p "$(${pkgs.coreutils}/bin/dirname "$output_file")"
-                    ${pkgs.pandoc}/bin/pandoc -f markdown -t html "$md_file" -o "$output_file"
+
+                    ${pkgs.coreutils}/bin/echo "<!DOCTYPE html>"              > "$output_file"
+                    ${pkgs.coreutils}/bin/echo "<html>"                      >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "<head>"                      >> "$output_file"
+                    ${pkgs.coreutils}/bin/cat "${components.head}"           >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "</head>"                     >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "<body>"                      >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "<header>"                    >> "$output_file"
+                    ${pkgs.coreutils}/bin/cat "${components.header}"         >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "</header>"                   >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "<main>"                      >> "$output_file"
+                    ${pkgs.pandoc}/bin/pandoc -f markdown -t html "$md_file" >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "</main>"                     >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "<footer>"                    >> "$output_file"
+                    ${pkgs.coreutils}/bin/cat "${components.footer}"         >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "</footer>"                   >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "</body>"                     >> "$output_file"
+                    ${pkgs.coreutils}/bin/echo "</html>"                     >> "$output_file"
                   done
                 '';
               in
