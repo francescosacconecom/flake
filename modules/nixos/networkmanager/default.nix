6f139d47a1fe3aa920b0d86a282862731c70e89b
Francesco Saccone <francesco@francescosaccone.com>
2025-02-25 22:19:09 +0100

    [networkmanager] Add randomiseMacAddress option

--- modules/nixos/networkmanager/default.nix
+++ modules/nixos/networkmanager/default.nix
@@ -11,11 +11,22 @@
       default = false;
       type = lib.types.bool;
     };
+    randomiseMacAddress = lib.mkOption {
+      description = "Whether to randomise the MAC address of each interface.";
+      default = false;
+      type = lib.types.bool;
+    };
   };
 
   config = lib.mkIf config.modules.networkmanager.enable {
-    networking.networkmanager = {
-      enable = true;
-    };
+    networking.networkmanager =
+      let
+        inherit (config.modules.networkmanager) randomiseMacAddress;
+      in
+      {
+        enable = true;
+        wifi.macAddress = if randomiseMacAddress then "random" else "preserve";
+        ethernet.macAddress = if randomiseMacAddress then "random" else "preserve";
+      };
   };
 }
