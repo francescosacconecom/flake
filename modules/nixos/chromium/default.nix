92ef2e77d49aaf526d80ac7d024007613c614123
Francesco Saccone <francesco@francescosaccone.com>
2025-02-26 17:20:06 +0100

    [chromium] Remove Searx dependency

--- modules/nixos/chromium/default.nix
+++ modules/nixos/chromium/default.nix
@@ -19,50 +19,40 @@
       pkgs.ungoogled-chromium
     ];
 
-    programs.chromium =
-      let
-        inherit (config.modules) searx;
-      in
-      rec {
-        enable = true;
+    programs.chromium = rec {
+      enable = true;
 
-        defaultSearchProviderEnabled = true;
-        defaultSearchProviderSuggestURL = "http://localhost:${builtins.toString searx.port}/search?q={searchTerms}";
-        defaultSearchProviderSearchURL = "http://localhost:${builtins.toString searx.port}/search?q={searchTerms}";
+      defaultSearchProviderEnabled = true;
 
-        homepageLocation =
-          if searx.enable then
-            "localhost:${builtins.toString config.modules.searx.port}"
-          else
-            "https://duckduckgo.com/";
+      homepageLocation = "https://duckduckgo.com/";
 
-        initialPrefs = {
-          autofill = false;
-          autologin = false;
-          bookmark_bar = {
-            show_apps_shortcut = false;
-            show_managed_bookmarks = false;
-            show_on_all_tabs = false;
-            show_tab_groups = false;
-          };
-          browser = {
-            enable_spellchecking = false;
-            has_seen_welcome_page = true;
-            show_home_button = false;
-          };
-          history = {
-            deleting_enabled = true;
-            saving_disabled = true;
-          };
-          homepage_is_newtabpage = true;
+      initialPrefs = {
+        autofill = false;
+        autologin = false;
+        bookmark_bar = {
+          show_apps_shortcut = false;
+          show_managed_bookmarks = false;
+          show_on_all_tabs = false;
+          show_tab_groups = false;
         };
-
-        extraOpts = {
-          BrowserSignin = 0;
-          SyncDisabled = true;
-          PasswordManagerEnabled = false;
-          SpellcheckEnabled = false;
+        browser = {
+          enable_spellchecking = false;
+          has_seen_welcome_page = true;
+          show_home_button = false;
         };
+        history = {
+          deleting_enabled = true;
+          saving_disabled = true;
+        };
+        homepage_is_newtabpage = true;
       };
+
+      extraOpts = {
+        BrowserSignin = 0;
+        SyncDisabled = true;
+        PasswordManagerEnabled = false;
+        SpellcheckEnabled = false;
+      };
+    };
   };
 }
