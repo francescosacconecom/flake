e06750e963665a3045deb3ed106277ee44e23ccc
Francesco Saccone <francesco@francescosaccone.com>
2025-02-27 19:07:47 +0100

    [francesco] Add some RSS feeds

--- homes/francesco/home/default.nix
+++ homes/francesco/home/default.nix
@@ -102,16 +102,70 @@
     newsraft = {
       enable = true;
       feeds = {
-        "World" = [
+        "Italy" = [
           {
-            name = "The Washington Post World";
-            url = "https://feeds.washingtonpost.com/rss/world";
+            name = "ANSA";
+            url = "https://www.ansa.it/sito/ansait_rss.xml";
           }
+          {
+            name = "Fanpage";
+            url = "https://www.fanpage.it/feed";
+          }
+          {
+            name = "Libero Quotidiano";
+            url = "https://www.liberoquotidiano.it/rss.xml";
+          }
+          {
+            name = "Repubblica.it";
+            url = "https://www.repubblica.it/rss/homepage/rss2.0.xml";
+          }
+        ];
+        "Technology" = [
+          {
+            name = "Dark Reading";
+            url = "https://www.darkreading.com/rss.xml";
+          }
+          {
+            name = "The Hacker News";
+            url = "https://feeds.feedburner.com/TheHackersNews";
+          }
+        ];
+        "World" = [
           {
             name = "CNN International";
             url = "http://rss.cnn.com/rss/edition.rss";
           }
+          {
+            name = "The Washington Post World";
+            url = "https://feeds.washingtonpost.com/rss/world";
+          }
         ];
+        "YouTube" =
+          [
+            {
+              name = "Mental Outlaw";
+              id = "UC7YOGHUfC1Tb6E4pudI9STA";
+            }
+            {
+              name = "Linus Tech Tips";
+              id = "UCXuqSBlHAE6Xw-yeJA0Tunw";
+            }
+            {
+              name = "The Game Theorists";
+              id = "UCo_IB5145EVNcf8hw1Kku7w";
+            }
+            {
+              name = "Veritasium";
+              id = "UCHnyfMqiRRG1u-2MsSQLbXA";
+            }
+          ]
+          |> builtins.map (
+            { name, id }:
+            {
+              inherit name;
+              url = "https://youtube.com/feeds/videos.xml?channel_id=${id}";
+            }
+          );
       };
     };
     sway = {
