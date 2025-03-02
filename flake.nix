d1094c0cb428d428781b10bef167363b897a4740
Francesco Saccone <francesco@francescosaccone.com>
2025-03-02 19:08:21 +0100

    Downgrade nixpkgs and home-manager to release 24.11

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs";
+      url = "github:nixos/nixpkgs/release-24.11";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix = {
